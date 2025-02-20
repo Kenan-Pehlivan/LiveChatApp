e5a024657be12d9104cff9aecf5b11951f5ab3d4
Kenan-Pehlivan <kenankardelen@outlook.de>
2025-02-20 21:59:33 +0100

    Create CHannel

--- server/routes/ContactRoutes.js
+++ server/routes/ContactRoutes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyToken } from "../middlewares/AuthMiddleware.js";
-import { getContactsforDMList, searchContacts } from "../controllers/ContactsController.js";
+import { getAllContacts, getContactsforDMList, searchContacts } from "../controllers/ContactsController.js";
 
 
 
@@ -8,5 +8,6 @@ const contactsRoutes = Router();
 
 contactsRoutes.post("/search", verifyToken, searchContacts);
 contactsRoutes.get("/get-contacts-for-dm", verifyToken, getContactsforDMList);
+contactsRoutes.get("/get-all-contacts", verifyToken, getAllContacts)
 
 export default contactsRoutes;
