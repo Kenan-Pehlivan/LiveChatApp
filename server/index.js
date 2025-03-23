dbd0309497c235ade14d3b149070b5123f158212
Chan-XOXO <singh.chanpreet.b@gmail.com>
2025-03-23 19:57:18 +0100

    unnötiges gelöscht
    test

--- server/index.js
+++ server/index.js
@@ -8,11 +8,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
-//import authRoutes from "./routes/AuthRoutes.js";
-//import contactsRoutes from "./routes/ContactRoutes.js";
 import setupSocket from "./socket.js";
-//import messagesRoutes from "./routes/MessagesRoutes.js";
-//import channelRoutes from "./routes/ChannelRoutes.js";
 
 //Alle Enviroment Variablen sind somit in Prosses.env (.env)
 dotenv.config();
@@ -41,11 +37,6 @@ app.use("//upload/files", express.static("/upload/files"));
 app.use(cookieParser());
 app.use(express.json());
 
-//API-Endpoints werden hier definiert
-//app.use("/api/auth", authRoutes);
-//app.use("/api/contacts", contactsRoutes);
-//app.use('/api/messages', messagesRoutes);
-//app.use("/api/channel", channelRoutes);
 
 //Jedes mal das der Server gestartet wird, gebe diese Meldung
 const server = app.listen(port, () => {
--- server/models/MessagesModel.js
+++
@@ -1,4 +0,0 @@
-/*  Veränderungsdatum: 21.03.2025 
-    Diese Datei enthält das Mongoose-Datenmodell für Nachrichten, einschließlich Sender, Empfänger, Nachrichtentyp und Inhalt.
-*/
-
